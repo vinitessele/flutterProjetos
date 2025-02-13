a536af0042d278547b200b3fad12b0e6c4ac2cf2
vinitessele <vinicius_tessele@hotmail.com>
2025-02-13 17:36:37 -0300

    first commit

---
+++ app_push_notifications/android/app/src/main/kotlin/com/example/app_push_notifications/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.app_push_notifications
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ app_push_notifications/lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('Firebase Notifications')),
+        body: NotificationWidget()//Center(child: Text('Hello World')),
+      ),
+    );
+  }
+}
+class NotificationWidget extends StatefulWidget {
+  @override
+  _NotificationWidgetState createState() => _NotificationWidgetState();
+}
+class _NotificationWidgetState extends State<NotificationWidget> {
+  final FirebaseMessaging _firebaseMessaging = FirebaseMessaging.instance;
+  @override
+  void initState() {
+    super.initState();
+    _firebaseMessaging.requestPermission();
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      print('Message data: ${message.data}');
+      if (message.notification != null) {
+        print('Message also contained a notification: ${message.notification}');
+      }
+    });
+    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
+      print('Message clicked! ${message.messageId}');
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Waiting for messages'),
+    );
+  }
+}
